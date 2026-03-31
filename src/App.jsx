9d771ad06bec9be5a02da69fcd312cabec29c138
Azhar Zisan <azharzisanxtr@gmail.com>
2026-03-31 16:31:09 +0100

    Nav and total expense design completed. Chart.js is registered and imported not completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import "./App.css";
+import { IoMdAdd } from "react-icons/io";
+import LineChart from "./components/LineChart";
 
 function App() {
-
   return (
     <>
-      
+      <nav className="w-full h-auto fixed top-0 z-10000 text-center p-4 bg-[#ffb3c1] text-[#590d22] text-4xl font-bold border-b-2 border-[#590d22]">
+        Expense Tracker
+      </nav>
+      {/* section for total expenses */}
+      <section className="w-full h-auto p-4 mt-18">
+        <div className="w-full h-auto flex justify-between items-center rounded-xl bg-[#800f2f] py-2 px-4">
+          <div className="flex flex-col justify-center items-start">
+            <h2 className="text-lg font-semibold text-[#ffccd5]">
+              Total Expenses
+            </h2>
+            <p className="text-4xl text-[#fff0f3]">$76749.86</p>
+          </div>
+          {/* duration filter */}
+          <button className="w-auto h-full flex justify-center items-center gap-1 text-xl font-semibold text-[#800f2f] bg-[#ffccd5] pr-2 pl-3 py-3 rounded hover:bg-[#ff8fa3] cursor-pointer ">
+            Add <IoMdAdd className="text-2xl font-semibold hover:rotate-360" />
+          </button>
+        </div>
+      </section>
+      {/* section for charts and bars */}
+      <section>
+        <div className="flex flex-col justify-center items-center bg-green-300">
+          <div className="max-w-[50%] w-100% bg-white p-5 m-7"></div>
+          <LineChart />
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart,
+  Filler,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+
+Chart.register(
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  RadialLinearScale,
+  Filler,
+);
+
+const LineChart = () => {
+  return;
+};
+
+export default LineChart;
